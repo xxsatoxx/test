73fced54cefa3671ea7af534f806a1305b283ea5
xxsatoxx <118036949+xxsatoxx@users.noreply.github.com>
2023-02-19 13:04:52 +0900

    Add files via upload

---
+++ apps/detector/forms.py
@@ -0,0 +1,25 @@
+from flask_wtf.file import FileAllowed, FileField, FileRequired
+from flask_wtf.form import FlaskForm
+from wtforms.fields.simple import SubmitField
+
+
+class UploadImageForm(FlaskForm):
+    # ファイルフィールドに必要なバリデーションを設定する
+    image = FileField(
+        validators=[
+            FileRequired("画像ファイルを指定してください。"),
+            FileAllowed(["png", "jpg", "jpeg"], "サポートされていない画像形式です。"),
+        ]
+    )
+    submit = SubmitField("アップロード")
+
+
+class DetectorForm(FlaskForm):
+    submit = SubmitField("検知")
+
+
+class DeleteForm(FlaskForm):
+    submit = SubmitField("画像削除")
+
+# class DeletagForm(FlaskForm):
+#    submit = SubmitField("タグ削除")
